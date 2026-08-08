0ace7981899472bef1bce92b0173479517c9e344
Joshua Chaparro <yosoyjosh2000@gmail.com>
2026-08-07 21:56:40 -0600

    0217 contains-duplicate: set, O(n)

---
+++ solutions/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+"""
+217. contains duplicate · https://leetcode.com/problems/contains-duplicate/
+
+Patrón:
+Señal:
+Tiempo:   Espacio:
+Intento:
+Repaso:   2026-08-10
+"""
+
+
+class Solution:
+    def
